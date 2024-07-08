bacf98a79d18b18c915e4e98330cdbe1e6ea05b4
Lucas Henrique <henrique.lucas521@gmail.com>
2024-07-07 22:19:10 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Response } from "express"
+import cors from 'cors'
+
+const port = 8080
+const url = `http://localhost:${port}`
+
+const app = express()
+
+app.use(cors())
+app.use(express.json({ limit: '10mb' }))
+
+const healthCheckRouter = express.Router();
+
+healthCheckRouter.route('/health').get(async (_, res: Response) => {
+  res.send({ status: "ok" });
+});
+
+app.use('/', healthCheckRouter)
+
+app.listen(port, () => {
+  console.log(`🟢 servidor iniciado em ${url} 🟢`)
+})
+
