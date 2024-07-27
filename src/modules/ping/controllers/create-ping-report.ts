d1927989ec6bb354553bdb31f96d52d57fad87e8
Lucas Henrique <henrique.lucas521@gmail.com>
2024-07-27 00:45:08 -0300

    add datetime field to ping and traceroute report models

--- src/modules/ping/controllers/create-ping-report.ts
+++ src/modules/ping/controllers/create-ping-report.ts
@@ -3,4 +3,11 @@ import { Request, Response } from "express";
 
 export const create = (req: Request, res: Response) => {
   const body = req.body as Prisma.PingReportCreateInput;
+
+  try {
+  } catch (error) {
+    return res.status(500).json({
+      message: { error: (error as Error).message },
+    });
+  }
 };
