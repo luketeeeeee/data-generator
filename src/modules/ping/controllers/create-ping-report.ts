d8eb36895aa1fb003a047066708f40a26299bf13
Lucas Henrique <henrique.lucas521@gmail.com>
2024-07-27 01:30:11 -0300

    add ping report create route

--- src/modules/ping/controllers/create-ping-report.ts
+++ src/modules/ping/controllers/create-ping-report.ts
@@ -1,10 +1,34 @@
-import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
+import { createPingReport } from "../ping.services";
+import { exec } from "child_process";
 
-export const create = (req: Request, res: Response) => {
-  const body = req.body as Prisma.PingReportCreateInput;
+type PingReportBody = {
+  isFlood: boolean;
+  timeLimit: number;
+  ipAddress: string;
+};
+
+export const create = async (req: Request, res: Response) => {
+  const { isFlood, timeLimit, ipAddress } = req.body as PingReportBody;
+
+  exec(
+    `ping ${isFlood && "-f"} -w ${timeLimit} -i 0.002 ${ipAddress}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log({ error: (error as Error).message });
+        return false;
+      }
+
+      if (stderr) {
+        console.log({ stderr: `${stderr}` });
+      }
+    },
+  );
+
+  // const pingReportObject = {}
 
   try {
+    // const newPingReport = await createPingReport();
   } catch (error) {
     return res.status(500).json({
       message: { error: (error as Error).message },
--- src/modules/ping/ping.routes.ts
+++ src/modules/ping/ping.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { pingController } from "./controllers";
 
 const router = express.Router();
 
 // router.route('/').get(pingController.findAll)
+router.route("/").post(pingController.create);
 
 export default router;
