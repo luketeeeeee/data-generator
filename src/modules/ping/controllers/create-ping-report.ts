c669adbb3f883e7bf6df663206087323dc8e340f
Lucas Henrique <henrique.lucas521@gmail.com>
2024-08-17 18:45:40 -0300

    return stdout on array format

--- src/modules/ping/controllers/create-ping-report.ts
+++ src/modules/ping/controllers/create-ping-report.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
-import { createPingReport } from "../ping.services";
 import { exec } from "child_process";
+import { promisify } from "util";
+import { createPingReport } from "../ping.services";
+import { validateIPAddress } from "../../../utils/validateIpAddress";
 
 type PingReportBody = {
   isFlood: boolean;
   timeLimit: number;
   ipAddress: string;
 };
+
+const execPromise = promisify(exec);
 
 export const create = async (req: Request, res: Response) => {
   const { isFlood, timeLimit, ipAddress } = req.body as PingReportBody;
@@ -17,28 +21,30 @@ export const create = async (req: Request, res: Response) => {
     });
   }
 
-  exec(
-    `ping ${isFlood && "-f"} -w ${timeLimit} -i 0.002 ${ipAddress}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log({ error: (error as Error).message });
-        return false;
-      }
-
-      if (stderr) {
-        console.log({ stderr: `${stderr}` });
-      }
-
-      console.log(stdout);
-    },
-  );
-
-  // const pingReportObject = {}
-
   try {
-    // const newPingReport = await createPingReport();
+    const { stdout, stderr } = await execPromise(
+      `ping ${isFlood ? "-f" : ""} -w ${timeLimit} -i 0.002 ${ipAddress}`,
+    );
+
+    if (stderr) {
+      // Implement a way to save the error history
+      // model PingError { id, errorString }
+      console.log(stderr);
+      return res.status(400).json({
+        success: false,
+      });
+    }
+
+    const resultArray = stdout.split("\n");
+    resultArray.splice(1, 1);
+
+    return res.status(200).json({
+      success: true,
+      data: resultArray,
+    });
   } catch (error) {
     return res.status(500).json({
+      success: true,
       message: { error: (error as Error).message },
     });
   }
