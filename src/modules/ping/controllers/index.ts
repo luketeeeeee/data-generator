7b5c7d53ecaf2bb207a64d953f3be92c1807864d
Lucas Henrique <henrique.lucas521@gmail.com>
2024-08-18 02:48:33 -0300

    add find all ping reports route

--- src/modules/ping/controllers/index.ts
+++ src/modules/ping/controllers/index.ts
@@ -1,5 +1,7 @@
+import { findAll } from "./find-all-ping-reports";
 import { create } from "./create-ping-report";
 
 export const pingReportController = {
+  findAll,
   create,
 };
--- src/modules/ping/ping.routes.ts
+++ src/modules/ping/ping.routes.ts
@@ -3,7 +3,7 @@ import { pingReportController } from "./controllers";
 
 const router = express.Router();
 
-// router.route('/').get(pingController.findAll)
+router.route("/").get(pingReportController.findAll);
 router.route("/").post(pingReportController.create);
 
 export default router;
