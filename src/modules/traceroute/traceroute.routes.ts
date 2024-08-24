80a7dbdd7a724a6933cabe2f14371115e949ad45
Lucas Henrique <henrique.lucas521@gmail.com>
2024-08-24 14:59:34 -0300

    last commit was a mistake

---
+++ src/modules/traceroute/traceroute.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+// import {  } from "./controllers";
+
+const router = express.Router();
+
+// router.route("/").get(pingReportController.findAll);
+// router.route("/").post(pingReportController.create);
+
+export default router;
---
+++ src/modules/traceroute/traceroute.services.ts
@@ -0,0 +1,33 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../../prisma";
+
+export const findAllTracerouteReports = () => {
+  return prisma.tracerouteReport.findMany({});
+};
+
+export const findTracerouteReportByIpAddress = (ipAddress: string) => {
+  return prisma.tracerouteReport.findMany({ where: { ipAddress } });
+};
+
+export const findTracerouteReportById = (id: string) => {
+  return prisma.tracerouteReport.findUnique({ where: { id } });
+};
+
+export const createTracerouteReport = (data: Prisma.TracerouteReportCreateInput) => {
+  return prisma.tracerouteReport.create({ data });
+};
+
+export const updateTracerouteReport = (
+  id: string,
+  data: Prisma.TracerouteReportUpdateInput,
+) => {
+  return prisma.tracerouteReport.update({ where: { id }, data });
+};
+
+export const deleteTracerouteReport = (id: string) => {
+  return prisma.tracerouteReport.delete({ where: { id } });
+};
+
+export const deleteTracerouteReportsByIpAddress = (ipAddress: string) => {
+  return prisma.tracerouteReport.deleteMany({ where: { ipAddress } });
+};
